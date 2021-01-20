06dd73008226bf0781e30f5225d12a1ade3409d1
mchwalfajar <53744120+mchwalfajar@users.noreply.github.com>
2021-01-20 16:00:56 +0700

    update credential jeager

--- jeager/credential.c
+++ jeager/credential.c
@@ -3,8 +3,8 @@ String ACCESSKEY = "a08c0faa1b988b11:9ae97d4dee93a723";
 String deviceID = "5ee83817c9771900175209f7/TestDrive";
 #define app "5ee83817c9771900175209f7"
 #define deviceName "TestDrive"
-#define WIFISSID "KOS-LT3"
-#define PASSWORD "KW33LBLT3"
+#define WIFISSID "wifi-ssid"
+#define PASSWORD "wifi-password"
 
 const char apn[] = "Telkomsel";
 const char gprsUser[] = "wap";
